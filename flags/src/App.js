10c7107cbaacfc71a56263a7b2748fdac54d2cd6
ajaykumar-21 <ajaykumarpk789@gmail.com>
2024-02-25 15:26:51 +0530

    complete all flags card

--- flags/src/App.js
+++ flags/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [countries, setContries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => setContries(data))
+      .catch((error) => console.error("Error fetching data: ", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {countries.map((country) => (
+        <div className="cardStyles">
+          <img
+            src={country.flags.png}
+            alt={`Flag of ${country.name.common}`}
+            style={{ width: "100px", height: "100px" }}
+          />
+          <h3>{country.name.common}</h3>
+        </div>
+      ))}
     </div>
   );
 }
